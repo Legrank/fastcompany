e1f9f5858b61782b047e46cbcc62fa484a8a704e
legrank <flyu.gyu@yandex.ru>
2023-01-11 08:45:39 +0300

    add inc/dec metods

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,37 +1,52 @@
-import React, { useState } from "react";
-import Counter from "./counter";
+import React, { useState } from 'react'
+import Counter from './counter'
 
 const CountersList = () => {
-    const initialState = [
-        { id: 0, value: 0, name: "Ненужная вещь", price: "200" },
-        { id: 1, value: 4, name: "Ложка" },
-        { id: 2, value: 0, name: "Вилка" },
-        { id: 3, value: 0, name: "Тарелка" },
-        { id: 4, value: 0, name: "Набор минималиста" },
-    ];
+  const initialState = [
+    { id: 0, value: 0, name: 'Ненужная вещь', price: '200' },
+    { id: 1, value: 4, name: 'Ложка' },
+    { id: 2, value: 0, name: 'Вилка' },
+    { id: 3, value: 0, name: 'Тарелка' },
+    { id: 4, value: 0, name: 'Набор минималиста' },
+  ]
 
-    const [counters, setCounters] = useState(initialState);
-    const handleDelete = (id) => {
-        const newCounters = counters.filter((c) => c.id !== id);
-        setCounters(newCounters);
-    };
-    const handleReset = () => {
-        setCounters(initialState);
-        console.log("handle reset");
-    };
+  const [counters, setCounters] = useState(initialState)
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((c) => c.id !== id)
+    setCounters(newCounters)
+  }
+  const handleIncrement = (id) => {
+    const newCounters = counters.map((counter) => {
+      if (counter.id === id) {
+        counter.value++
+      }
+      return counter
+    })
+    setCounters(newCounters)
+  }
 
-    return (
-        <>
-            {counters.map((count) => (
-                <Counter key={count.id} onDelete={handleDelete} {...count} />
-            ))}
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleReset}
-            >
-                Сброс
-            </button>
-        </>
-    );
-};
-export default CountersList;
+  const handleDecrement = (id) => {
+    const newCounters = counters.map((counter) => {
+      if (counter.id === id) {
+        counter.value--
+      }
+      return counter
+    })
+    setCounters(newCounters)
+  }
+
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
+        />
+      ))}
+    </>
+  )
+}
+export default CountersList
